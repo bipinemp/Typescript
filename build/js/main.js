f30b088e0b13c8c6424ef3ad5fb1a365ec7d1298
bipinemp <bhandaribipin3.39@gmail.com>
2023-05-07 19:08:55 +0545

    firsts commit

---
+++ build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let myName = "Bipin";
+let meaningOfLife;
+let isLoading;
+let album;
+myName = "Bipin";
+meaningOfLife = 0;
+isLoading = true;
+album = 1010;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+let myName: string = "Bipin";
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+myName = "Bipin";
+meaningOfLife = 0;
+isLoading = true;
+album = 1010;
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g;
