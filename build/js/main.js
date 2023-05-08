2724c24f132d802608b3a6b9e8849fa065dae46d
bipinemp <bhandaribipin3.39@gmail.com>
2023-05-08 18:30:21 +0545

    changes

--- build/js/main.js
+++ build/js/main.js
@@ -1,71 +1,24 @@
 "use strict";
-// Literal types
-let myName;
-let userName;
-userName = "Dave";
-// functions
-const add = (a, b) => {
-    return a + b;
+//Assertions
+// Convert to more or less specific
+let a = "hello";
+let b = a; // less specific
+let c = a; // more specific
+let d = "world";
+let e = "world";
+const addOrConcat = (a, b, c) => {
+    if (c === "add")
+        return a + b;
+    return "" + a + b;
 };
-const logMsg = (message) => {
-    console.log(message);
-};
-logMsg("hello");
-// logMsg(add(1, 3));
-let subtract = function (c, d) {
-    return c - d;
-};
-// interface mathFunction {
-//   (a: number, b: number): number;
-// }
-let multiply = function (c, d) {
-    return c * d;
-};
-// logMsg(multiply(3, 2));
-// optional parameters
-const addAll = (a, b, c) => {
-    if (typeof c !== "undefined") {
-        return a + b + c;
-    }
-    return a + b;
-};
-// default param value
-const sumALl = (a = 2, b, c = 2) => {
-    return a + b + c;
-};
-// logMsg(addAll(1, 2, 3));
-// logMsg(addAll(1, 2));
-// logMsg(sumALl(1, 2));
-// logMsg(sumALl(undefined, 2)); we have to send undefined if we use a default as first parameter otherwise it is used in last same for optional
-// Rest parameters
-const total = (a, ...nums) => {
-    return a + nums.reduce((prev, curr) => prev + curr);
-};
-// logMsg(total(1, 2, 3, 4));
-const createError = (errMsg) => {
-    throw new Error(errMsg);
-};
-const infinite = () => {
-    let i = 1;
-    while (true) {
-        i++;
-        if (i > 100)
-            break;
-    }
-};
-// custom type guard
-const isNumber = (value) => {
-    return typeof value === "number" ? true : false;
-};
-const isString = (value) => {
-    return typeof value === "string" ? true : false;
-};
-// use of the never type
-const numberOrString = (value) => {
-    if (isString(value))
-        return "string";
-    if (isNumber(value))
-        return "number";
-    return createError("This should never happen");
-};
-logMsg(numberOrString("67"));
+let myVal = addOrConcat(2, 2, "concat");
+// Be careful TS sees no problem - but a string is returned
+let nextVal = addOrConcat(2, 2, "concat");
+// 10 as string; shows error if possible
+10;
+// The DOM
+const img = document.querySelector("img");
+const myImg = document.getElementById("#img");
+const nextImg = document.getElementById("#img");
+img.src;
+myImg.src;
--- src/main.ts
+++ src/main.ts
@@ -1,101 +1,38 @@
-// Type Aliases
-type StringOrNumber = string | number;
-type StringOrNumberArray = (string | number)[];
+type One = string;
+type Two = string | number;
+type Three = "hello";
 
-type Guitarist = {
-  name?: String;
-  active: boolean;
-  albums: StringOrNumberArray;
+//Assertions
+
+// Convert to more or less specific
+let a: One = "hello";
+let b = a as Two; // less specific
+let c = a as Three; // more specific
+
+let d = <One>"world";
+let e = <string | number>"world";
+
+const addOrConcat = (
+  a: number,
+  b: number,
+  c: "add" | "concat"
+): number | string => {
+  if (c === "add") return a + b;
+  return "" + a + b;
 };
 
-type UserId = StringOrNumber;
+let myVal: string = addOrConcat(2, 2, "concat") as string;
 
-// Literal types
-let myName: "Bipin";
+// Be careful TS sees no problem - but a string is returned
+let nextVal: number = addOrConcat(2, 2, "concat") as number;
 
-let userName: "Dave" | "John" | "Amy";
-userName = "Dave";
+// 10 as string; shows error if possible
+10 as unknown as string;
 
-// functions
-const add = (a: number, b: number): number => {
-  return a + b;
-};
+// The DOM
+const img = document.querySelector("img")!;
+const myImg = document.getElementById("#img") as HTMLImageElement;
+const nextImg = <HTMLImageElement>document.getElementById("#img");
 
-const logMsg = (message: any): void => {
-  console.log(message);
-};
-
-logMsg("hello");
-// logMsg(add(1, 3));
-
-let subtract = function (c: number, d: number): number {
-  return c - d;
-};
-
-// logMsg(subtract(3, 1));
-
-type mathFunction = (a: number, b: number) => number;
-// interface mathFunction {
-//   (a: number, b: number): number;
-// }
-
-let multiply: mathFunction = function (c, d) {
-  return c * d;
-};
-
-// logMsg(multiply(3, 2));
-
-// optional parameters
-const addAll = (a: number, b: number, c?: number): number => {
-  if (typeof c !== "undefined") {
-    return a + b + c;
-  }
-  return a + b;
-};
-
-// default param value
-const sumALl = (a: number = 2, b: number, c: number = 2): number => {
-  return a + b + c;
-};
-
-// logMsg(addAll(1, 2, 3));
-// logMsg(addAll(1, 2));
-// logMsg(sumALl(1, 2));
-
-// logMsg(sumALl(undefined, 2)); we have to send undefined if we use a default as first parameter otherwise it is used in last same for optional
-
-// Rest parameters
-const total = (a: number, ...nums: number[]): number => {
-  return a + nums.reduce((prev, curr) => prev + curr);
-};
-
-// logMsg(total(1, 2, 3, 4));
-
-const createError = (errMsg: string): never => {
-  throw new Error(errMsg);
-};
-
-const infinite = () => {
-  let i: number = 1;
-  while (true) {
-    i++;
-    if (i > 100) break;
-  }
-};
-
-// custom type guard
-const isNumber = (value: any): boolean => {
-  return typeof value === "number" ? true : false;
-};
-const isString = (value: any): boolean => {
-  return typeof value === "string" ? true : false;
-};
-
-// use of the never type
-const numberOrString = (value: number | string): string => {
-  if (isString(value)) return "string";
-  if (isNumber(value)) return "number";
-  return createError("This should never happen");
-};
-
-logMsg(numberOrString("67"));
+img.src;
+myImg.src;
